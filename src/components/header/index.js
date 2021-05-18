f1e750425054fcf70e103b3ab6301089675aec3a
Stefan Roets <stefanroetscode@gmail.com>
2021-05-18 11:53:45 +0200

    Link Routes and Pages in Next.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 function Header () {
     return (
         <header className="text-gray-400 bg-gray-900 body-font">
@@ -6,13 +8,15 @@ function Header () {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round"
                          strokeLinejoin="round" strokeWidth="2"
                          className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/>
                     </svg>
                     <span className="ml-3 text-xl">Tailblocks</span>
                 </a>
                 <nav
                     className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-white">First Link</a>
+                    <Link href="/about">
+                        <a className="mr-5 hover:text-white">About</a>
+                    </Link>
                     <a className="mr-5 hover:text-white">Second Link</a>
                     <a className="mr-5 hover:text-white">Third Link</a>
                     <a className="mr-5 hover:text-white">Fourth Link</a>
